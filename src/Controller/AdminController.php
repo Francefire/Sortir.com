2359c2d38f36879f94fe4a5bbae8c285ebd5b2cd
Maxime <>
2024-04-24 10:49:41 +0200

    Création de la partie administration

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin', name: 'app_')]
+class AdminController extends AbstractController
+{
+    #[Route('', name: 'admin')]
+    public function admin(): Response
+    {
+        return $this->render('admin/admin.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    #[Route('/create_user', name: 'create_user')]
+    public function create_user(): Response
+    {
+
+    }
+}
