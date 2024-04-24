f0aff9fa30047f998b20ce54766843e7f0bb94f3
Maxime <>
2024-04-24 12:01:42 +0200

    Ajouts de la gestion des utilisateurs dans la partie administration
    - Création
    
    - Modification
    
    - Suppression

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\EditUserType;
+use App\Form\RegistrationFormType;
+use App\Repository\CampusRepository;
+use App\Repository\UserRepository;
+use App\Security\AppAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/admin', name: 'app_')]
@@ -17,9 +27,80 @@ class AdminController extends AbstractController
         ]);
     }
 
-    #[Route('/create_user', name: 'create_user')]
-    public function create_user(): Response
+    #[Route('/users', name: 'admin_users')]
+    public function users(UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAll();
 
+        return $this->render('admin/users.html.twig', [
+            'controller_name' => 'AdminController',
+            'users' => $users
+        ]);
+    }
+
+    #[Route('/users/create', name: 'admin_users_create')]
+    public function users_create(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager, CampusRepository $campusRepository): Response
+    {
+        $user = new User();
+        $user->setAdministrator(false);
+        $user->setActive(false);
+        $registrationForm = $this->createForm(RegistrationFormType::class, $user);
+        $registrationForm->handleRequest($request);
+
+        if ($registrationForm->isSubmitted() && $registrationForm->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $registrationForm->get('plainPassword')->getData()
+                )
+            );
+            $user->setCampus($campusRepository->find(1)); // Default campus for admin //TODO: DELETE THIS LINE
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Compte crée avec succès');
+            return $this->redirectToRoute('app_admin_users');
+        }
+
+        return $this->render('admin/users_create.html.twig', [
+            'controller_name' => 'AdminController',
+            'registrationForm' => $registrationForm,
+        ]);
+    }
+
+    #[Route('/users/edit/{id}', name: 'admin_users_edit')]
+    public function users_edit(Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository, UserPasswordHasherInterface $userPasswordHasher, int $id): Response
+    {
+        $user = $userRepository->find($id);
+
+        $editForm = $this->createForm(EditUserType::class, $user);
+
+        $editForm->handleRequest($request);
+        if($editForm->isSubmitted() && $editForm->isValid()) {
+            if (!empty($user->getPlainPassword())) {
+                $user->setPassword($userPasswordHasher->hashPassword($user, $user->getPlainPassword()));
+            }
+            $entityManager->flush();
+            $this->addFlash('success', 'Profil modifié avec succès');
+            return $this->redirectToRoute('app_admin_users');
+        }
+
+        return $this->render('admin/users_edit.html.twig', [
+            'controller_name' => 'AdminController',
+            'editForm' => $editForm->createView(),
+            'user' => $user
+        ]);
+    }
+
+    #[Route('/users/delete/{id}', name: 'admin_users_delete')]
+    public function users_delete(EntityManagerInterface $em, UserRepository $userRepository, int $id): Response
+    {
+        $user = $userRepository->find($id);
+        $em->remove($user);
+        $em->flush();
+
+        $this->addFlash('success', 'Compte supprimé avec succès');
+        return $this->redirectToRoute('app_admin_users');
     }
 }
