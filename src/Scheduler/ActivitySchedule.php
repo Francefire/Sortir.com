aa7b72f37b6c59c5905702b261993e77fda69edb
Francefire <francefire.contact@gmail.com>
2024-04-29 14:44:07 +0200

    Etats des activités : Listeners refonte
    - Correction de comment on gere l'etat des activité, maintenant les activité sont update lorsqu'un user load l'activité

--- src/Scheduler/ActivitySchedule.php
+++ src/Scheduler/ActivitySchedule.php
@@ -19,9 +19,14 @@ final class ActivitySchedule implements ScheduleProviderInterface
 
     public function getSchedule(): Schedule
     {
+        // @TODO - A Modifier, il faudra utiliser ca pour archiver les sorties, mais pas les etats, ou verifier les etats moins souvent
         return (new Schedule())
             ->add(
-                RecurringMessage::every('1 minute', new Message\CheckState())
+                RecurringMessage::every(
+                    '1 minute',
+                    new Message\CheckState(),
+                    from: new \DateTimeImmutable('13:00', new \DateTimeZone('Europe/Paris'))
+                )
             )
             ->stateful($this->cache)
         ;
